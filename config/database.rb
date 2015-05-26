667c196a697affd3fcfae76e7ce5e417ddf756eb
Whitney Hiemstra <whitcolorado@gmail.com>
2015-05-26 13:46:10 -0600

    added rake to Gemfile

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,7 @@
 # set the database based on the current environment
-database_name = "task-manager-#{TaskManager.environment}"
-
+database_name = "task_manager_#{TaskManager.environment}"      # = "task_manager_test b/c ENV["RACK_ENV"] ||= "test"
+                                                               # = "task_manager_development b/c ENV["RACK_ENV"] ||= "development"
+                                                               # = "task_manager_production b/c ENV["RACK_ENV"] ||= "production"
 # connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
   :adapter  => "sqlite3",
